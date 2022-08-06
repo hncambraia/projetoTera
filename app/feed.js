f6aa1955d6e8558676efb50c709b2f78482bb5cf
henriquecambraia <henriquecambraia@falconi.com>
2022-08-06 17:20:34 -0300

    Impressão e pesquisa de amigos. pesquisa e cadastro de post

--- app/feed.js
+++ app/feed.js
@@ -1,64 +1,86 @@
 let feed = [
     {
-        "id":1,
-        "titulo":"titulo 1",
-        "texto":"texto 1 <br>texto 1213",        
-        "imagem":"img/cafe.jpg",
-        "idusuario":"1"
+        "id": 1,
+        "titulo": "titulo 1",
+        "texto": "texto 1 <br>texto 1213",
+        "imagem": "img/cafe.jpg",
+        "idusuario": "1"
     },
     {
-        "id":2,
-        "titulo":"titulo 2",
-        "texto":"texto 2",        
-        "imagem":"img/cafe2.jpg",
-        "idusuario":"1"
+        "id": 2,
+        "titulo": "titulo 2",
+        "texto": "texto 2",
+        "imagem": "img/cafe2.jpg",
+        "idusuario": "1"
     },
     {
-        "id":3,
-        "titulo":"titulo 3",
-        "texto":"texto 3",        
-        "imagem":"img/cafe2.jpg",
-        "idusuario":"2"
-    }   ,
+        "id": 3,
+        "titulo": "titulo 3",
+        "texto": "texto 3",
+        "imagem": "img/cafe2.jpg",
+        "idusuario": "2"
+    },
     {
-        "id":4,
-        "titulo":"titulo 4",
-        "texto":"texto 4",        
-        "imagem":"img/cafe2.jpg"        ,
-        "idusuario":"3"
-    }   ,
+        "id": 4,
+        "titulo": "titulo 4",
+        "texto": "texto 4",
+        "imagem": "img/cafe2.jpg",
+        "idusuario": "3"
+    },
     {
-        "id":5,
-        "titulo":"titulo 5",
-        "texto":"texto 5",        
-        
-        "idusuario":"1"
-    }   ,
+        "id": 5,
+        "titulo": "titulo 5",
+        "texto": "texto 5",
+        "idusuario": "1"
+    },
     {
-        "id":6,
-        "titulo":"titulo 6",
-        "texto":"texto 6"        
-    }   
+        "id": 6,
+        "titulo": "titulo 6",
+        "texto": "texto 6"
+    }
 
 ]
-let text=""
-var invalidEntries = 0;
 
-var feedFiltrado = feed.filter(function(el){
-    return el.idusuario== location.search.substring(1) || el.idusuario == undefined;
-})
+function imprimeFeed(id, pesquisa) {
 
-
-for (var index =0; index < feed.length;index++)   {   
-    var post=""
-    post = "<div class='posts'> <h1>" + feedFiltrado[index].titulo +"</h1>" + 
-                "<h3>" + feedFiltrado[index].texto+"</h3>         <br>    <input id='pesquisa'> <br> <button id='botaoComentar'>Comentar</button><br></div>" 
-               
-    if (feedFiltrado[index].imagem != undefined)
-        {   
-            post ="<div class='posts'> <h1>" + feedFiltrado[index].titulo +"</h1>" + 
-            "<h3>" + feedFiltrado[index].texto+"</h3>"+
-                "<img src='" + feedFiltrado[index].imagem +"'</img>   <br>  <input id='pesquisa'> <br> <button id='botaoComentar'>Comentar</button><br></div>"
+    var feedFiltrado = feed.filter(function (el) {
+        return el.idusuario == id || el.idusuario == undefined;
+    })
+    document.getElementById('postsFeed').innerHTML = ""
+    console.log(pesquisa)
+    for (var index = 0; index < feed.length; index++) {
+        var post = ""
+        console.log(feedFiltrado[index].titulo)
+        if (feed[index].texto.includes(pesquisa) || feed[index].titulo.includes(pesquisa) || pesquisa == undefined) {
+            post = "<div class='posts'> <h1>" + feedFiltrado[index].titulo + "</h1>" +
+                "<h3>" + feedFiltrado[index].texto + "</h3>         <br>    <input id='comentario'> <br> <button id='botaoComentar'>Comentar</button><br></div>"
+            console.log(feedFiltrado[index].texto)
+            if (feedFiltrado[index].imagem != undefined) {
+                post = "<div class='posts'> <h1>" + feedFiltrado[index].titulo + "</h1>" +
+                    "<h3>" + feedFiltrado[index].texto + "</h3>" +
+                    "<img src='" + feedFiltrado[index].imagem + "'</img>   <br>  <input id='comentario'> <br> <button id='botaoComentar'>Comentar</button><br></div>"
+            }
+            document.getElementById('postsFeed').innerHTML += post;
         }
-    document.getElementById('postsFeed').innerHTML += post;
+    }
+}
+
+
+function pesquisaFeed() {
+    var idUsuario = location.search.substring(1);
+    imprimeFeed(idUsuario, document.getElementById("pesquisaFeed").value)
+}
+
+
+function postarFeed() {
+    var novoFeed = {
+        "id": feed.length + 1,
+        "titulo": "Titulo " + document.getElementById("postar").value,
+        "texto": document.getElementById("postar").value,
+        "idusuario": location.search.substring(1)
+    }
+
+    feed.push(novoFeed)
+    console.log(feed)
+    pesquisaFeed()
 }
--- app/usuarios.js
+++ app/usuarios.js
@@ -1,83 +1,86 @@
 var usuarios = [
     {
-        "id":1,
-        "nome":"Henrique Nogueira Cambraia",
-        "login":"hncambraia",
-        "email":"hncambraia@gmail.com",
+        "id": 1,
+        "nome": "Henrique Nogueira Cambraia",
+        "login": "hncambraia",
+        "email": "hncambraia@gmail.com",
         "senha": "12345678",
-        "bio":"henrique nogueira",
-        "foto":"img/1.jpg"
+        "bio": "henrique nogueira",
+        "foto": "img/1.jpg",
+        "amigos": [2, 3, 4]
     },
     {
-        "id":2,
-        "nome":"Ueslei",
-        "login":"ueslei",
-        "email":"ueslei@gmail.com",
+        "id": 2,
+        "nome": "Ueslei",
+        "login": "ueslei",
+        "email": "ueslei@gmail.com",
         "senha": "12345678",
-        "bio":"ueslei",
-        "foto":"img/2.jpg"
+        "bio": "ueslei",
+        "foto": "img/2.jpg",
+        "amigos": [1, 3]
     },
     {
-        "id":3,
-        "nome":"Braulio",
-        "login":"Braulio",
-        "email":"Braulio@gmail.com",
+        "id": 3,
+        "nome": "Braulio",
+        "login": "Braulio",
+        "email": "Braulio@gmail.com",
         "senha": "12345678",
-        "bio":"Braulio",
-        "foto":"img/3.jpg"
-    }   
+        "bio": "Braulio",
+        "foto": "img/3.jpg",
+        "amigos": [1, 2, 4]
+    }
     ,
     {
-        "id":4,
-        "nome":"Tetsuo Matsumura",
-        "login":"tetsuo",
-        "email":"tetsuomatsumura@tera.com",
+        "id": 4,
+        "nome": "Tetsuo Matsumura",
+        "login": "tetsuo",
+        "email": "tetsuomatsumura@tera.com",
         "senha": "12345678",
-        "bio":"Tetsuo Matsumura",
-        "foto":"img/4.jpg"
-    }   
+        "bio": "Tetsuo Matsumura",
+        "foto": "img/4.jpg",
+        "amigos": [1, 2, 3]
+
+    }
 ]
 
-function validaLogin (usuario, senha){
+function validaLogin(usuario, senha) {
     var usuarioLocalizado = false;
     var senhaValidado = false;
-    var mensagemFinal ="";
-    var usuarioValidado ;
-    
-    for (var index =0; index < usuarios.length;index++)   {
-    if (usuarios[index].login==usuario)
-    {       
-        usuarioLocalizado = true;           
-        if(usuarios[index].senha==senha){
-              
-            senhaValidado = true;
-            usuarioValidado = usuarios[index].id;
-            break;        
-        }        
-    }    
-   }
-   
-   if (usuarioLocalizado && senhaValidado)
-   {
-       window.location.href = "feed.html?" + usuarioValidado;
-        return ["",usuarioValidado];
-   }
+    var mensagemFinal = "";
+    var usuarioValidado;
 
-//    else if (senhaValidado == false && usuarioLocalizado){
-//             mensagemFinal = "senha invalida"    
-//    }
-   else{
-    mensagemFinal = "Usuário ou senha inválidos!"    
-   }
-   
-    window.alert (mensagemFinal);
-   
+    for (var index = 0; index < usuarios.length; index++) {
+        if (usuarios[index].login == usuario) {
+            usuarioLocalizado = true;
+            if (usuarios[index].senha == senha) {
+
+                senhaValidado = true;
+                usuarioValidado = usuarios[index].id;
+                break;
+            }
+        }
+    }
+
+    if (usuarioLocalizado && senhaValidado) {
+        window.location.href = "feed.html?" + usuarioValidado;
+        return ["", usuarioValidado];
+    }
+
+    //    else if (senhaValidado == false && usuarioLocalizado){
+    //             mensagemFinal = "senha invalida"    
+    //    }
+    else {
+        mensagemFinal = "Usuário ou senha inválidos!"
+    }
+
+    window.alert(mensagemFinal);
+
 }
 
-function imprimeDadosUsuarios(id){
-    
-    var usuarioFiltrado = usuarios.filter(function(el){
-        return el.id== id;
+function imprimeDadosUsuarios(id) {
+
+    var usuarioFiltrado = usuarios.filter(function (el) {
+        return el.id == id;
     })
     console.log(usuarioFiltrado)
     document.getElementById('nome').textContent = "Nome: " + usuarioFiltrado[0].nome
@@ -85,11 +88,45 @@ function imprimeDadosUsuarios(id){
     document.getElementById('email').textContent = "E-mail: " + usuarioFiltrado[0].email
     document.getElementById('bio').textContent = "Bio: " + usuarioFiltrado[0].bio
     document.getElementById('avatar').src = usuarioFiltrado[0].foto
+    imprimeListaAmigos(id,"")
+
     getUser(id)
 }
+function imprimeListaAmigos(idUsuario,pesquisa) {
+
+    var amigos = ""
+    var usuarioFiltrado = usuarios.filter(function (el) {
+        return el.id == idUsuario;
+    })
+
+    document.getElementById('listaAmigos').innerHTML=""
+
+    for (var indexAmigos = 0; indexAmigos < usuarioFiltrado[0].amigos.length; indexAmigos++) {
+        console.log(usuarioFiltrado[0].amigos[indexAmigos])
+
+        var amigoFiltrado = usuarios.filter(function (el) {
+            return el.id == usuarioFiltrado[0].amigos[indexAmigos];
+        })
+
+        if(amigoFiltrado[0].nome.includes(pesquisa) ||amigoFiltrado[0].email.includes(pesquisa) || pesquisa == undefined)
+        {
+        amigos = "<div class='amigo'><img class='imgAmigo' src='" + amigoFiltrado[0].foto +"'</img> <h5>" + amigoFiltrado[0].nome + " ("+ amigoFiltrado[0].email + ")</h5> </div>"
+
+        document.getElementById('listaAmigos').innerHTML += amigos
+        }
+    }
 
 
-function getUser(id){
-    document.getElementById('urlProfile').href = "profile.html?"+id
-    document.getElementById('urlFeed').href = "feed.html?"+id
+
+}
+
+function pesquisaAmigos()
+{
+    var idUsuario = location.search.substring(1);
+    imprimeListaAmigos(idUsuario,document.getElementById("pesquisa").value)
+}
+
+function getUser(id) {
+    document.getElementById('urlProfile').href = "profile.html?" + id
+    document.getElementById('urlFeed').href = "feed.html?" + id
 }
